16b829fc06c1ff0adfe16a2a1159714933de9ce1
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-06-26 01:17:43 -0500

    Exercise 1.19 done

--- chapter1/exercise1_19.c
+++ chapter1/exercise1_19.c
@@ -1,5 +1,5 @@
 /*
- * Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines.
+ * Write a function reverse(s) that reverses the character string s. Use it to write a program that reverses its input a line at a time.
  *
  * W., Kernighan Brian; Ritchie Dennis. C Programming Language (p. 31). Pearson Education. Kindle Edition.
  */
@@ -8,22 +8,35 @@
 #define MAXLINE 1000 /* maximum input line size */
 
 int get_line(char line[], int maxline);
-void rm_trailing_blanks(char line[], int len);
+int rm_trailing_blanks(char line[], int len);
+void reverse(char line[], int len);
 
 int main() {
   int len;                /* current line length */
   char line[MAXLINE];     /* current input line  */
 
   while ((len = get_line(line, MAXLINE)) > 0) {
-    rm_trailing_blanks(line, len);
+    len = rm_trailing_blanks(line, len);
+    reverse(line, len);
     printf("%s", line);
   }
 
   return 0;
 }
 
-/* rm_trailing_blanks: removes trailing blanks from a string */
-void rm_trailing_blanks(char s[], int len) {
+/* reverse: given a string and its length, it reverses it */
+void reverse(char line[], int len) {
+  int counter, buff;
+  for (counter = 0; counter < len - 2; ++counter){
+    buff = line[counter];
+    line[counter] = line[len-2];
+    line[len - 2] = buff;
+    --len;
+  }
+}
+
+/* rm_trailing_blanks: removes trailing blanks from a string, returns new size */
+int rm_trailing_blanks(char s[], int len) {
   int ni; // New index
   for (ni = len - 1; s[ni] == ' ' || s[ni] == '\t' || s[ni] == '\n'; --ni);
   if (ni != -1) {
@@ -31,6 +44,7 @@ void rm_trailing_blanks(char s[], int len) {
     ++ni;
   }
   s[ni + 1] = '\0';
+  return ni + 1;
 }
 
 /* get_line: read a line into s, return length */
