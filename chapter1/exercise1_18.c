2e280a7ce4e8f21cfec0772eeaf99ff266c3387f
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-06-25 21:48:16 -0500

    Se terminó el ejercicio 1.18

--- chapter1/exercise1_18.c
+++ chapter1/exercise1_18.c
@@ -8,39 +8,41 @@
 #define MAXLINE 1000 /* maximum input line size */
 
 int get_line(char line[], int maxline);
+void rm_trailing_blanks(char line[], int len);
 
 int main() {
-  int len;    /* current line length */
-  char line[MAXLINE];     /* current input line */
+  int len;                /* current line length */
+  char line[MAXLINE];     /* current input line  */
 
   while ((len = get_line(line, MAXLINE)) > 0) {
+    rm_trailing_blanks(line, len);
     printf("%s", line);
   }
 
   return 0;
 }
 
+/* rm_trailing_blanks: removes trailing blanks from a string */
+void rm_trailing_blanks(char s[], int len) {
+  int ni; // New index
+  for (ni = len - 1; s[ni] == ' ' || s[ni] == '\t' || s[ni] == '\n'; --ni);
+  if (ni != -1) {
+    s[ni + 1] = '\n';
+    ++ni;
+  }
+  s[ni + 1] = '\0';
+}
+
 /* get_line: read a line into s, return length */
 int get_line(char s[], int lim) {
-  int c, i, j, s_index, sc;
-
-  sc = 0;
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    if (c == ' ') ++sc;
-    else if (c != ' ') {
-      s_index = i - sc;
-      for (j = s_index; j < s_index + sc; ++j)
-        s[j] = '*';
-      s[i] = c;
-      sc = 0;
-    }
-  }
+  int c, nc;
+  for (nc = 0; nc < lim -1 && (c = getchar()) != EOF && c != '\n'; ++nc)
+    s[nc] = c; 
   if (c == '\n') {
-    if ((i - sc) != 0) s[i - sc] = c;
-    else s[0] = '\b';
-    ++i;
+    s[nc] = c;
+    ++nc;
   }
-  s[i - sc] = '\0';
-  
-  return i;
+  s[nc] = '\0';
+  return nc;
 }
+
